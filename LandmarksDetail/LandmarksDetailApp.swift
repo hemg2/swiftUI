7206b144e3944888aa76fa56f08d488643fadf62
hemg2 <hemg2@naver.com>
2022-11-14 13:53:34 +0900

    Initial Commit

---
+++ LandmarksDetail/LandmarksDetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksDetailApp.swift
+//  LandmarksDetail
+//
+//  Created by 1 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksDetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
